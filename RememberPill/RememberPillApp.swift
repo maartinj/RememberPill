5f61c3eb548dc748b096928848f994a354c76d9b
Marcin <mjedrzejak@icloud.com>
2022-07-13 02:45:57 +0200

    no message

---
+++ RememberPill/RememberPillApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberPillApp.swift
+//  RememberPill
+//
+//  Created by Marcin Jędrzejak on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RememberPillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
