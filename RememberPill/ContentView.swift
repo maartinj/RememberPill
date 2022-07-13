1a55eb531946d6a8994b1a3085e8ed54dde0092c
Marcin <mjedrzejak@icloud.com>
2022-07-13 02:52:09 +0200

    Corrected hello world and added new text

--- RememberPill/ContentView.swift
+++ RememberPill/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world")
+                .padding(10)
+            Text("New text")
         }
     }
 }
